60f83b8ee7ef97eef5a0d555999098324fe6acf0
sharda <shardakumari96115@gmail.com>
2026-08-25 19:49:22 +0530

    JavaScript

---
+++ .vscode/JavaScript/Rest.js
@@ -0,0 +1,7 @@
+let even = [2,4,6,8,10];
+// let first = even[0];
+// let second = even[1];
+let[first,second,...bachGaya] = even;
+console.log('first', first);
+console.log('second', second);
+console.log('bachGya', bachGaya);
