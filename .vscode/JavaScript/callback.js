a6ec76debfe4033b695197cb8c1d8290ce964b98
sharda <shardakumari96115@gmail.com>
2026-08-26 18:37:08 +0530

    Callback

---
+++ .vscode/JavaScript/callback.js
@@ -0,0 +1,14 @@
+function hello(name) {
+  console.log('Hello ' + name);
+
+}
+function capitalize(name){
+  let upper = name.toUpperCase();
+  console.log(upper);
+}
+function processUserInput(process) {
+let studentName = prompt('Enter you name: ');
+process(studentName);
+}
+processUserInput(hello);
+processUserInput(capitalize)
